1b1b084bd22431e15a0578f09388909c8788667c
PiusHwang <bk22106@gmail.com>
2024-10-20 22:30:24 +0900

    redoc to "/api/", qr_utils.py, add api docs navbar link

--- core/urls.py
+++ core/urls.py
@@ -25,7 +25,12 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("pius_hwang/", admin.site.urls),
-    path("", include("qr.urls")),
+    path("", include("qr.urls", namespace="qr")),
+    
+    # API Documentation
+    path(
+        "api/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ]
 
 if settings.DEBUG:
@@ -45,8 +50,5 @@ if settings.DEBUG:
             "swagger/",
             schema_view.with_ui("swagger", cache_timeout=0),
             name="schema-swagger-ui",
-        ),
-        path(
-            "redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
         ),
     ]
