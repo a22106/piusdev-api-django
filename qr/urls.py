cca20cd152a5a7b93fe6c9ea986f37fb727dbc3e
PiusHwang <bk22106@gmail.com>
2024-11-02 23:57:37 +0900

    qrcode 기능 추가(sms, bitcoin, whatsapp, mecard, event, geo)

--- qr/urls.py
+++ qr/urls.py
@@ -10,6 +10,12 @@ from .views import (
     QrPhoneNumberView,
     QrVcardView,
     QrWifiView,
+    QrSmsView,
+    QrGeoView,
+    QrEventView,
+    QrMeCardView,
+    QrWhatsAppView,
+    QrBitcoinView,
 )
 
 from rest_framework import permissions
@@ -41,6 +47,12 @@ urlpatterns = [
     path("v1/qr/phonenumber/", QrPhoneNumberView.as_view(), name="qr_phone_number_v1"),
     path("v1/qr/vcard/", QrVcardView.as_view(), name="qr_vcard_v1"),
     path("v1/qr/wifi/", QrWifiView.as_view(), name="qr_wifi_v1"),
+    path("v1/qr/sms/", QrSmsView.as_view(), name="qr_sms_v1"),
+    path("v1/qr/geo/", QrGeoView.as_view(), name="qr_geo_v1"),
+    path("v1/qr/event/", QrEventView.as_view(), name="qr_event_v1"),
+    path("v1/qr/mecard/", QrMeCardView.as_view(), name="qr_mecard_v1"),
+    path("v1/qr/whatsapp/", QrWhatsAppView.as_view(), name="qr_whatsapp_v1"),
+    path("v1/qr/bitcoin/", QrBitcoinView.as_view(), name="qr_bitcoin_v1"),
 ]
 
 if settings.DEBUG:
