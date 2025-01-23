9a95fcf9a2c94b72c0217752824c3f287cce7f39
PiusHwang <bk22106@gmail.com>
2025-01-24 08:52:08 +0900

    [feat] QR 단순 모델 생성

--- apps/qr/models.py
+++ apps/qr/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from pydantic import BaseModel
+from datetime import datetime
+class QrStyles(BaseModel):
+    name: str
+    description: str
+    created_at: datetime
+    updated_at: datetime
 
-# Create your models here.
