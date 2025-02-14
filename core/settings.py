47d5ce43c571e5c8c83e78294334f5cc5124c8c9
PiusHwang <bk22106@gmail.com>
2025-02-15 02:30:09 +0900

    [Config] Add Heroku URL to CSRF Trusted Origins

--- core/settings.py
+++ core/settings.py
@@ -58,6 +58,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5085",
     'http://localhost:5173',
     "https://qrcode.piusdev.com",
+    f"https://{os.environ.get('HEROKU_URL')}",
 ]
 
 ALLOWED_HOSTS = [
